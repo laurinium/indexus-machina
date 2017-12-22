25874b765a12f08720fc2d7dc3f0b5e42548b3c0
Jens Laur <laur@secra.de>
2017-12-22 15:54:17 +0100

    added stuff

--- src/manager/index.php
+++ src/manager/index.php
@@ -7,3 +7,19 @@
  *
  * Beschreibung: ${Desc}
  */
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
